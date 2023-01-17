02ee244aeda45feafa3ddc0efd9712e2be43f95e
Park SeongYong <sh__y_@naver.com>
2023-01-17 11:20:24 +0900

    deploy test

--- Routes/pipeline.js
+++ Routes/pipeline.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 var express = require('express')
 var router = express.Router();
 
@@ -42,34 +43,44 @@ router.post('/', (req, res, next) => {
     })
 })
 
-router.get('/:pipelineID', (req, res, next) => {
-
-    if(!req.body.date) {
-        res.send({result: "FAIL", msg: 'select date is required'})
-        return;
-    }
-
-    var date = req.body.date;
-    const dateRegex = /^\d{4}\-\d{2}\-\d{2}$/;
-    let extractResult = date.match(dateRegex);
-    if(!extractResult) {
-        res.send({result: "FAIL", msg: 'date value is not valid'})
-        return;
-    }
-
+router.get('/additions', (req, res, next) => {
     getConnection((conn) => {
-        var sql = 'SELECT a.pipelineID, b.deptName, c.customerName, d.typeName, a.title, a.date, a.content, a.createAt, a.modifyAt, expectedSales, expectedPurchase, expectedProfit' 
+        var sql = 'SELECT deptID, deptName FROM DEPT ORDER BY deptName; '
+        sql = sql + 'SELECT customerID, customerName FROM CUSTOMER ORDER BY customerName; '
+        sql = sql + 'SELECT typeID, typeName FROM TYPE ORDER BY typeName'
+
+        conn.query(sql, (err, rows, fields) => {
+            if(err) {
+                logger.error('Request /pipeline/additions : Select "Related Pipeline inputs" has problem\n' + err)
+                res.send({result: "FAIL", msg: '[ERROR] Select "Related Pipeline inputs" has problem\n' + err})
+            } else {
+                logger.info('Request /pipeline/additions : SUCCESS')
+                res.send({result: "SUCCESS", depts: rows[0], customers: rows[1], types: rows[2]})
+            }
+        })
+
+        conn.release();
+    })
+})
+
+router.get('/:pipelineID', (req, res, next) => {
+    getConnection((conn) => {
+        var sql = 'SELECT a.pipelineID, b.deptName, c.customerName, d.typeName, a.title, a.date, a.createAt, a.modifyAt, expectedSales, expectedPurchase, expectedProfit' 
         var sql = sql + ' FROM PIPELINE a, DEPT b, CUSTOMER c, TYPE d'
         var sql = sql + ' WHERE a.deptID = b.deptID AND a.customerID = c.customerID AND a.typeID = d.typeID'
-        var sql = sql + ' AND a.date = ? AND a.pipelineID = ?;'
-        var param = [date, req.params.pipelineID];
+        var sql = sql + ' AND a.pipelineID = ?;'
+        var sql = sql + ' SELECT seqnum, font, color, content'
+        var sql = sql + ' FROM PIPELINE_CONTENTS'
+        var sql = sql + ' WHERE pipelineID = ? ORDER BY seqnum'
+        var param = [req.params.pipelineID, req.params.pipelineID];
 
         conn.query(sql, param, (err, rows, fields) => {
             if(err) {
-                console.log('[ERROR] Select Contents "Related Pipeline" has problem\n' + err)
+                logger.error('Request /pipeline/' + req.params.pipelineID + ' : Select Contents "Related Pipeline" has problem\n' + err)
                 res.send({result: "FAIL", msg: '[ERROR] Select Contents "Related Pipeline" has problem\n' + err})
             } else {
-                res.send({result: "SUCCESS", pipeline: rows[0]})
+                logger.info('Request /pipeline/' + req.params.pipelineID + ' : SUCCESS')
+                res.send({result: "SUCCESS", pipeline: rows[0], content: rows[1]})
             }
         })
     
@@ -93,9 +104,82 @@ router.post('/dateValidationCheck', (req, res, next) => {
 router.post('/create', (req, res, next) => {
     var body = req.body;
 
-    if(!body.deptID || !body.customerID || !body.typeID || !body.title || !body.date) {
-
+    if(!body.deptID || !body.customerID || !body.typeID || !body.title || !body.date || !body.expectedSales || !body.expectedPurchase || !body.expectedProfit) {
+        logger.error('Request /pipeline/create : please request with required informations (deptID, customerID, typeID, title, date, expectedSales, expectedPurchase, expectedProfit)')
+        res.send({result: "FAIL", msg: '[ERROR] please request with required informations (deptID, customerID, typeID, title, date)'})
+        return;
     }
+
+    var date = req.body.date;
+    const dateRegex = /^\d{4}\-\d{2}\-\d{2}$/;
+    let extractResult = date.match(dateRegex);
+    if(!extractResult) {
+        logger.error('Request /pipeline/create : date value is not valid')
+        res.send({result: "FAIL", msg: '[ERROR] date value is not valid'})
+        return;
+    }
+
+    var getLastID = new Promise((resolve, reject) => getConnection((conn) => {
+        var sql = 'SELECT max(pipelineID) as last_id FROM PIPELINE;'
+
+        conn.query(sql, (err, rows, fields) => {
+            if(err) {
+                reject(err);
+            } else {
+                var last_id = JSON.parse(JSON.stringify(rows[0])).last_id;
+                resolve(++last_id);
+            }
+        })
+
+        conn.release();
+    }))
+
+    var createPipeline = new Promise((resolve, reject) => getConnection((conn) => {
+        var sql = 'INSERT INTO PIPELINE(deptID, customerID, typeID, title, date, expectedSales, expectedPurchase, expectedProfit, createAt, modifyAt, isDelete) VALUES(?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW(), ?)'
+        var param = [body.deptID, body.customerID, body.typeID, body.title, body.date, body.expectedSales, body.expectedPurchase, body.expectedProfit, 'N'];
+
+        conn.query(sql, param, (err) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        })
+
+        conn.release();
+    }))
+
+    createPipeline.then(() => {
+        getLastID.then((last_id) => {
+            getConnection((conn) => {
+                var seqnum;
+                for(seqnum=1; seqnum<=req.body.contents.length; seqnum++) {
+                    var sql = 'INSERT INTO PIPELINE_CONTENTS(pipelineID, seqnum, font, color, content, createAt) VALUES(?, ?, ?, ?, ?, NOW())'
+                    var param = [last_id, seqnum, req.body.contents[seqnum-1].font, req.body.contents[seqnum-1].color, req.body.contents[seqnum-1].content]
+
+                    conn.query(sql, param, (err) => {
+                        if(err) {
+                            logger.error('Request /pipeline/create : Cannot create new Pipeline Content\n' + err)
+                            res.send({result: "FAIL", msg: '[ERROR] Cannot create new Pipeline Content\n' + err})
+
+                            conn.release();
+                            return;
+                        }
+                    })
+                }
+                logger.info('Request /pipeline/create : SUCCESS')
+                res.send({result: "SUCCESS"})
+
+                conn.release();
+            })
+        }, (err) => {
+            logger.error('Request /pipeline/create : DB Connection has problem\n' + err)
+            res.send({result: "FAIL", msg: '[ERROR] DB Connection has problem\n' + err})
+        })
+    }, (err) => {
+        logger.error('Request /pipeline/create : Cannot create New Pipeline\n' + err)
+        res.send({result: "FAIL", msg: '[ERROR] Cannot create New Pipeline\n' + err})
+    })
 })
 
 module.exports = router;
